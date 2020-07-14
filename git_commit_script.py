d674179518c030f56d6f93ce788b2410a414bf1b
Shaun Price <46876579+shaundnz@users.noreply.github.com>
2020-07-14 13:32:45 +1200

    Initial commit

---
+++ git_commit_script.py
@@ -0,0 +1,51 @@
+import os
+import subprocess
+import sys
+import argparse
+
+def set_args():
+    parser = argparse.ArgumentParser(
+        description="Create a new git repo and make commits to create art in Github's activity graph")
+    parser.add_argument("-N", "--name", type=str, required=False, default="dummy_repo",
+                        help="Name of new repo, creates new directory with same in CWD name to hold local repo")
+    return parser.parse_args()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    arguments = set_args()
+    dir_name = arguments.name
+
+    # Create directory for local repo and move into it
+    try:
+        os.makedirs(dir_name)
+        os.chdir(dir_name)
+    except FileExistsError:
+        return
+
+    # Init the git repo
+    subprocess.run(["git", "init"])
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
