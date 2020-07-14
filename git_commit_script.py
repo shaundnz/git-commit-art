e6454ceccf42b15e94979b81382963e70b185637
Shaun Price <shaundnz@gmail.com>
2020-07-14 17:23:54 +1200

    Automatic commit from list values implemented

--- git_commit_script.py
+++ git_commit_script.py
@@ -2,7 +2,10 @@ import os
 import subprocess
 import sys
 import argparse
-import datetime
+from datetime import datetime
+from datetime import timedelta
+
+commit_one_week = [1,2,3,4,5,6,7]
 
 def set_args():
     parser = argparse.ArgumentParser(
@@ -10,26 +13,18 @@ def set_args():
     parser.add_argument("-N", "--name", type=str, required=True,
                         help="Name of remote repo, creates new directory with same in CWD name to hold local repo, default is 'dummy_repo'")
     parser.add_argument("-U", "--username", type=str, required=True, help="GitHub username")
-    parser.add_argument("-D", "--startdate", type=datetime.datetime.fromisoformat, required=False, help="date to start commits, default is from Sunday 1 year ago")
+    parser.add_argument("-D", "--startdate", type=datetime.fromisoformat, required=False,
+                        default=datetime.today() - timedelta(366),
+                        help="first Sunday from date to start commits, default is from first Sunday 1 year ago - format YYYY-MM-DD")
     return parser.parse_args()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def get_first_sunday(date):
+    first_sunday = date
+    day_index = datetime.weekday(first_sunday)
+    while (day_index < 6):
+        first_sunday = first_sunday + timedelta(1)
+        day_index = datetime.weekday(first_sunday)
+    return first_sunday
 
 def main():
     arguments = set_args()
@@ -40,7 +35,12 @@ def main():
         os.makedirs(dir_name)
         os.chdir(dir_name)
     except FileExistsError:
+        print("File already exists")
         return
+
+
+    commit_date = get_first_sunday(arguments.startdate)
+
 
     # Init the git repo
     subprocess.run(["git", "init"])
@@ -48,16 +48,21 @@ def main():
     file = open(os.path.join(os.getcwd(), "README.md"), "a+")
     subprocess.run(["git", "add", "README.md"])
 
-    # Carry out commits
-    file.write("commit1")
-    subprocess.run(["git", "commit", "-m", "testing commit"])
 
-    # Ask for username and password
-    # subprocess.run(["git", "config", "--local credential.helper"])
+    # Carry out commits
+    for commit in commit_one_week:
+        for i in range(commit):
+            file = open(os.path.join(os.getcwd(), "README.md"), "a+")
+            file.write("test + \n")
+            subprocess.run(["git", "add", "."])
+            subprocess.run(["git", "commit", "-m", "testing commit", '--date', commit_date.strftime('"%Y-%m-%d %H:%M:%S"')])
+        commit_date = commit_date + timedelta(1)
+
 
     # Push the commits to remote repository
     remote_repo = "https://github.com/" + arguments.username + "/" + arguments.name + ".git"
-    subprocess.run(["git", "remote", "add", "origin", "git@github.com:" + arguments.username + "/" + arguments.name + ".git"])
+    subprocess.run(
+        ["git", "remote", "add", "origin", "https://github.com/" + arguments.username + "/" + arguments.name + ".git"])
     subprocess.run(["git", "push", "-u", "origin", "master"])
 
 
