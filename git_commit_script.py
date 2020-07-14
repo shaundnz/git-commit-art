ef85ff74ed902f3e4010db55174eb3dde175f6d1
Shaun Price <shaundnz@gmail.com>
2020-07-14 18:02:37 +1200

    Added space invader image and implemented commit for images requiring more than one week

--- git_commit_script.py
+++ git_commit_script.py
@@ -1,23 +1,40 @@
 import os
 import subprocess
-import sys
 import argparse
 from datetime import datetime
 from datetime import timedelta
 
 commit_one_week = [1,2,3,4,5,6,7]
+COMMIT_MULTIPLIER = 2
+
+SPACE_INVADER = [ [0, 0, 0, 1, 1, 1, 0],  # 0
+                  [0, 0, 1, 1, 0, 0, 0],  # 1
+                  [0, 1, 1, 1, 1, 1, 0],  # 2
+                  [1, 1, 0, 1, 1, 0, 1],  # 3
+                  [0, 1, 1, 1, 1, 0, 1],  # 4
+                  [0, 1, 1, 1, 1, 0, 0],  # 5
+                  [0, 1, 1, 1, 1, 0, 1],  # 6
+                  [1, 1, 0, 1, 1, 0, 1],  # 7
+                  [0, 1, 1, 1, 1, 1, 0],  # 8
+                  [0, 0, 1, 1, 0, 0, 0],  # 9
+                  [0, 0, 0, 1, 1, 1, 0] ] # 10
+
 
 def set_args():
     parser = argparse.ArgumentParser(
         description="Create a local git repo and make commits to create art in Github's activity graph")
+    # Repo name
     parser.add_argument("-N", "--name", type=str, required=True,
                         help="Name of remote repo, creates new directory with same in CWD name to hold local repo, default is 'dummy_repo'")
+    # GitHub username
     parser.add_argument("-U", "--username", type=str, required=True, help="GitHub username")
+    # Start date for image
     parser.add_argument("-D", "--startdate", type=datetime.fromisoformat, required=False,
                         default=datetime.today() - timedelta(366),
                         help="first Sunday from date to start commits, default is from first Sunday 1 year ago - format YYYY-MM-DD")
     return parser.parse_args()
 
+# Top left corner of any image created
 def get_first_sunday(date):
     first_sunday = date
     day_index = datetime.weekday(first_sunday)
@@ -26,11 +43,18 @@ def get_first_sunday(date):
         day_index = datetime.weekday(first_sunday)
     return first_sunday
 
+# Edit the README then commit the changes
+def change_and_commit(commit_date):
+    file = open(os.path.join(os.getcwd(), "README.md"), "a+")
+    file.write("making art + \n\n")
+    subprocess.run(["git", "add", "."])
+    subprocess.run(["git", "commit", "-m", "testing commit", '--date', commit_date.strftime('"%Y-%m-%d %H:%M:%S"')])
+
 def main():
     arguments = set_args()
-    dir_name = arguments.name
 
     # Create directory for local repo and move into it
+    dir_name = arguments.name
     try:
         os.makedirs(dir_name)
         os.chdir(dir_name)
@@ -38,29 +62,21 @@ def main():
         print("File already exists")
         return
 
-
+    # Starting date for all commits
     commit_date = get_first_sunday(arguments.startdate)
-
 
     # Init the git repo
     subprocess.run(["git", "init"])
-    # Create and open README.md, the add it to be versioned
-    file = open(os.path.join(os.getcwd(), "README.md"), "a+")
-    subprocess.run(["git", "add", "README.md"])
 
-
-    # Carry out commits
-    for commit in commit_one_week:
-        for i in range(commit):
-            file = open(os.path.join(os.getcwd(), "README.md"), "a+")
-            file.write("test + \n")
-            subprocess.run(["git", "add", "."])
-            subprocess.run(["git", "commit", "-m", "testing commit", '--date', commit_date.strftime('"%Y-%m-%d %H:%M:%S"')])
-        commit_date = commit_date + timedelta(1)
-
+    # Carry out specified number of commits per day
+    image = SPACE_INVADER
+    for week in image:
+        for day in week:
+            for i in range(day * COMMIT_MULTIPLIER):
+                change_and_commit(commit_date)
+            commit_date = commit_date + timedelta(1)
 
     # Push the commits to remote repository
-    remote_repo = "https://github.com/" + arguments.username + "/" + arguments.name + ".git"
     subprocess.run(
         ["git", "remote", "add", "origin", "https://github.com/" + arguments.username + "/" + arguments.name + ".git"])
     subprocess.run(["git", "push", "-u", "origin", "master"])
